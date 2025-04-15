764e55564c9c307e427285f019d92a13a204e6d0
shreyarnayak <shreyarnayak14@gmail.com>
2025-04-15 15:06:29 +0530

    azure pipeline example

---
+++ src/main/java/Lab04/lab04_by_08/App.java
@@ -0,0 +1,13 @@
+package Lab04.lab04_by_08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
